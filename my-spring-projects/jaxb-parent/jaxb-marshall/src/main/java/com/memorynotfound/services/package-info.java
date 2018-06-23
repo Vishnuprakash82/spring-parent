8ffdd2f981dc8e23c93a700eaf9eab041bc606ec
Vishnuprakash Rajagopalan <drvp1982@outlook.com>
2018-06-23 18:01:25 +0200

    multiple project commit

---
+++ my-spring-projects/jaxb-parent/jaxb-marshall/src/main/java/com/memorynotfound/services/package-info.java
@@ -0,0 +1,2 @@
+@javax.xml.bind.annotation.XmlSchema(namespace = "http://services.memorynotfound.com/", elementFormDefault = javax.xml.bind.annotation.XmlNsForm.QUALIFIED)
+package com.memorynotfound.services;
---
+++ my-spring-projects/jaxb-sample/src/main/java/com/vishnu/jaxb/gen/package-info.java
@@ -0,0 +1,2 @@
+@javax.xml.bind.annotation.XmlSchema(namespace = "http://www.vishnu.com/jaxb/gen", elementFormDefault = javax.xml.bind.annotation.XmlNsForm.QUALIFIED)
+package com.vishnu.jaxb.gen;
